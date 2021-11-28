2227c723371f34374a380864da30cee096ffd370
Amélia SZK <amelia.szk@protonmail.com>
2021-11-27 19:04:02 -0500

    Tried to work with isAdjacent

--- CoulAdj.pl
+++ CoulAdj.pl
@@ -1,6 +1,6 @@
-isAdjPixel(Row1, Col1, Row2, Col2) :-
-    (Row1 =\= Row2) # (Col1 =\= 2).
+%isAdjPixel(Row1, Col1, Row2, Col2) :-
+%    (Row1 =\= Row2) # (Col1 =\= 2).
 
 isAdjacent( pixel(Row1, Col1, Red1, Gre1, Blu1, Alp1),
             pixel(Row2, Col2, Red2, Gre2, Blu2, Alp2)) :-
-    isAdjPixel(Row1, Col1, Row2, Col2).
+    \+ Row1 == Row2.
