6372fa3f21a331ccbe6090e6f5678817ea512140
Amélia SZK <amelia.szk@protonmail.com>
2021-12-08 17:01:14 -0500

    Query for adj colour works

--- CoulAdj.pl
+++ CoulAdj.pl
@@ -34,5 +34,59 @@ pixel(    5,   4, 210,  20, 230, 244).
 % This works as expected:
 % ?- pixel(R1, C1, _, _, _, Al1), pixel(R2, C2, _, _, _, Al2), \+ Al1 #= Al2, R1 #= R2, C2 #= C1+1.
 
+diff_colour(Red1, Gre1, Blu1, Alp1, Red2, Gre2, Blu2, Alp2) :-
+    Red1 #\= Red2 ;
+    Gre1 #\= Gre2 ;
+    Blu1 #\= Blu2 ;
+    Alp1 #\= Alp2.
+
+% This "works", but also writes an output to say that some combination of
+%   of variables failed?
+% ?- pixel(R1, C1, Red1,Gre1,Blu1,Al1), pixel(R2, C2,Red2,Gre2,Blu2,Al2), diff_colour(Red1, Gre1, Blu1, Alp1, Red2, Gre2, Blu2, Alp2), R1 #= R2, C2 #= C1+1.
+
+same_colour(Red1, Gre1, Blu1, Alp1, Red2, Gre2, Blu2, Alp2) :-
+    Red1 #= Red2,
+    Gre1 #= Gre2,
+    Blu1 #= Blu2,
+    Alp1 #= Alp2.
+
+% This one works, and also doesn't write an output for failures:
+% ?- pixel(R1, C1, Red1,Gre1,Blu1,Al1), pixel(R2, C2,Red2,Gre2,Blu2,Al2), \+ same_colour(Red1, Gre1, Blu1, Alp1, Red2, Gre2, Blu2, Alp2), R1 #= R2, C2 #= C1+1.
+
+
+is_midRigh(R1, C1, R2, C2) :-
+    R2 #= R1,
+    C2 #= C1 + 1.
+
+is_botLeft(R1, C1, R2, C2) :-
+    R2 #= R1 + 1,
+    C2 #= C1 - 1.
+
+is_botCent(R1, C1, R2, C2) :-
+    R2 #= R1 + 1,
+    C2 #= C1.
+
+is_botRigh(R1, C1, R2, C2) :-
+    R2 #= R1 + 1,
+    C2 #= C1 + 1.
+
+
+% Also works:
+% ?- pixel(R1, C1, Red1,Gre1,Blu1,Al1), pixel(R2, C2,Red2,Gre2,Blu2,Al2), \+ same_colour(Red1, Gre1, Blu1, Alp1, Red2, Gre2, Blu2, Alp2), is_midRigh(R1,C1,R2,C2).
+
+is_neighbour(R1, C1, R2, C2) :-
+    is_midRigh(R1, C1, R2, C2).
+
+% Also works:
+% ?- pixel(R1, C1, Red1,Gre1,Blu1,Al1), pixel(R2, C2,Red2,Gre2,Blu2,Al2), \+ same_colour(Red1, Gre1, Blu1, Alp1, Red2, Gre2, Blu2, Alp2), is_neighbour(R1,C1,R2,C2).
+
+is_neigh(R1, C1, R2, C2) :-
+    is_midRigh(R1, C1, R2, C2);
+    is_botLeft(R1, C1, R2, C2);
+    is_botCent(R1, C1, R2, C2);
+    is_botRigh(R1, C1, R2, C2).
+
+% Also works:
+% ?- pixel(R1, C1, Red1,Gre1,Blu1,Al1), pixel(R2, C2,Red2,Gre2,Blu2,Al2), \+ same_colour(Red1, Gre1, Blu1, Alp1, Red2, Gre2, Blu2, Alp2), is_neigh(R1,C1,R2,C2).
 
 
