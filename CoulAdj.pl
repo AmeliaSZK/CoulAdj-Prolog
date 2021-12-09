558e3eed51fa1100510d564c2cf9b4a2b21ea53c
Amélia SZK <amelia.szk@protonmail.com>
2021-12-09 14:29:07 -0500

    Refactored is_neigh

--- CoulAdj.pl
+++ CoulAdj.pl
@@ -15,7 +15,7 @@ is_adjColour(Red1, Gre1, Blu1, Alp1, Red2, Gre2, Blu2, Alp2) :-
     pixel(R1, C1, Red1, Gre1, Blu1, Alp1),
     pixel(R2, C2, Red2, Gre2, Blu2, Alp2),
     \+ same_colour(Red1, Gre1, Blu1, Alp1, Red2, Gre2, Blu2, Alp2),
-    is_neigh(R1, C1, R2, C2).
+    is_neighbour(R1, C1, R2, C2).
 
 same_colour(Red1, Gre1, Blu1, Alp1, Red2, Gre2, Blu2, Alp2) :-
     Red1 #= Red2,
@@ -23,48 +23,20 @@ same_colour(Red1, Gre1, Blu1, Alp1, Red2, Gre2, Blu2, Alp2) :-
     Blu1 #= Blu2,
     Alp1 #= Alp2.
 
-is_neigh(R1, C1, R2, C2) :-
-    is_topLeft(R1, C1, R2, C2);
-    is_topCent(R1, C1, R2, C2);
-    is_topRigh(R1, C1, R2, C2);
-    is_midLeft(R1, C1, R2, C2);
-    is_midRigh(R1, C1, R2, C2);
-    is_botLeft(R1, C1, R2, C2);
-    is_botCent(R1, C1, R2, C2);
-    is_botRigh(R1, C1, R2, C2).
+is_neighbour(R1, C1, R2, C2) :-
+    % Top
+    (R2 #= R1 - 1, C2 #= C1 - 1); % Left
+    (R2 #= R1 - 1, C2 #= C1    ); % Cent
+    (R2 #= R1 - 1, C2 #= C1 + 1); % Righ
 
-is_topLeft(R1, C1, R2, C2) :-
-    R2 #= R1 - 1,
-    C2 #= C1 - 1.
+    % Mid
+    (R2 #= R1    , C2 #= C1 - 1); % Left
+    (R2 #= R1    , C2 #= C1 + 1); % Righ
 
-is_topCent(R1, C1, R2, C2) :-
-    R2 #= R1 - 1,
-    C2 #= C1.
-
-is_topRigh(R1, C1, R2, C2) :-
-    R2 #= R1 - 1,
-    C2 #= C1 + 1.
-
-is_midLeft(R1, C1, R2, C2) :-
-    R2 #= R1,
-    C2 #= C1 - 1.
-
-is_midRigh(R1, C1, R2, C2) :-
-    R2 #= R1,
-    C2 #= C1 + 1.
-
-is_botLeft(R1, C1, R2, C2) :-
-    R2 #= R1 + 1,
-    C2 #= C1 - 1.
-
-is_botCent(R1, C1, R2, C2) :-
-    R2 #= R1 + 1,
-    C2 #= C1.
-
-is_botRigh(R1, C1, R2, C2) :-
-    R2 #= R1 + 1,
-    C2 #= C1 + 1.
-
+    % Bot
+    (R2 #= R1 + 1, C2 #= C1 - 1); % Left
+    (R2 #= R1 + 1, C2 #= C1    ); % Cent
+    (R2 #= R1 + 1, C2 #= C1 + 1). % Righ
 
 print_TSV_file(Filename, AllAdjacencies) :-
     Header = ['r', 'g', 'b', 'a', 'adj_r', 'adj_g', 'adj_b', 'adj_a'],
