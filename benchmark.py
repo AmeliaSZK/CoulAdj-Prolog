b1544f434b7332adb8cccd1dbad2a70e20a8cb3c
Amélia SZK <amelia.szk@protonmail.com>
2021-12-09 19:56:46 -0500

    Parametrized run

--- benchmark.py
+++ benchmark.py
@@ -1,12 +1,25 @@
-import subprocess
+import subprocess, time, os.path
+
+swi_prolog_executable = "swi-prolog.swipl"
+couladj_prolog = "CoulAdj.pl"
+
+pixels_directory = "tests/knowledge-bases/"
+result_directory = "tests/results/"
+golden_results = "tests/golden.tsv"
 
 print("benchmark started")
 
+size = 1
+pixels_filename = f"pixels-size-{size}.pl"
+result_filename = f"result-size-{size}.tsv"
+pixels_path = os.path.join(pixels_directory, pixels_filename)
+result_path = os.path.join(result_directory, result_filename)
+
 subprocess.run(
-    ["swi-prolog.swipl", 
-    "CoulAdj.pl", 
-    "tests/knowledge-bases/pixels-size-p.pl", 
-    "tests/results/result-size-p.tsv"]).check_returncode()
+    [swi_prolog_executable, 
+    couladj_prolog, 
+    pixels_path, 
+    result_path]).check_returncode()
 
 
 
